b0ce822ee71c2fc029fc9ce5d58ebfbad327ea23
Mayank Jain <mjmayank@gmail.com>
2025-11-28 18:27:53 -0500

    Update the questions in the survey

--- app/config/questions.js
+++ app/config/questions.js
@@ -1,9 +1,11 @@
 // Primary interview questions
 // This will eventually be fetched from a database
 export const primaryQuestions = [
-  "what do you think happens after we die, if anything?",
-  "more of a comment but i was babysitting the other day and thinking wow, genai is gonna make our childhoods so different from yours. what are your thoughts on that?",
-  "what was something you did that felt very difficult at the time and now you look back and think - huh that wasn't even close to very difficult?",
+  "Ryan asked: Who should the Dem 2028 presidential nominees be",
+  "Ryan asked: What animal haven’t you seen that you most want to see, in captivity or wild",
+  "Anna asked: What is something you love, that everyone else hates?",
+  "Anna asked: What are you leaving behind in 2025?",
+  "Kahini asked: What’s a tradition you want to keep? What’s a tradition you want to make?",
   "what have you been up to over the last month?",
 ];
 
